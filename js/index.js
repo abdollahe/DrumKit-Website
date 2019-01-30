5ca6eec88326d2c243a35225de73820dd770c07e
abdollahebadi <abdollah.ebadi@gmail.com>
2019-01-30 16:15:52 +1100

    play sound code reused

--- js/index.js
+++ js/index.js
@@ -4,11 +4,23 @@ for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++ ) {
     document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
     //alert("Button is clicked mate") ;
 
-    switch(this.innerHTML) {
-      case 'w':
-        var audio = new Audio('sounds/crash.mp3') ;
-        audio.play() ;
-        break ;
+    playSound(this.innerHTML) ;
+
+  }) ;
+}
+
+document.addEventListener("keydown" , function(e) {
+   playSound(e.key) ;
+
+}) ;
+
+
+function playSound(keyToPlay) {
+  switch(keyToPlay) {
+    case 'w':
+      var audio = new Audio('sounds/crash.mp3') ;
+      audio.play() ;
+      break ;
       case 'a':
         audio = new Audio('sounds/kick-bass.mp3') ;
         audio.play() ;
@@ -35,44 +47,5 @@ for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++ ) {
           break ;
       default:
         break ;
-    }
-
-
-    })
+  }
 }
-
-document.addEventListener("keydown" , function(e) {
-    switch(e.key) {
-      case 'w':
-        var audio = new Audio('sounds/crash.mp3') ;
-        audio.play() ;
-        break ;
-        case 'a':
-          audio = new Audio('sounds/kick-bass.mp3') ;
-          audio.play() ;
-          break ;
-        case 's':
-          audio = new Audio('sounds/snare.mp3') ;
-          audio.play() ;
-          break ;
-        case 'd':
-          audio = new Audio('sounds/tom-1.mp3') ;
-          audio.play() ;
-          break ;
-        case 'j':
-          audio = new Audio('sounds/tom-2.mp3') ;
-          audio.play() ;
-          break ;
-        case 'k':
-            audio = new Audio('sounds/tom-3.mp3') ;
-            audio.play() ;
-            break ;
-        case 'l':
-            audio = new Audio('sounds/tom-4.mp3') ;
-            audio.play() ;
-            break ;
-        default:
-          break ;
-    }
-
-}) ;
