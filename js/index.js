bfa85e27563f3b491340076db981d2f4afbd5826
abdollahebadi <abdollah.ebadi@gmail.com>
2019-01-30 16:09:53 +1100

    background of bottons changed -sound files playing with on screen click -key eventlistener added to the website

--- js/index.js
+++ js/index.js
@@ -2,6 +2,77 @@
 
 for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++ ) {
     document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
-      alert("Button is clicked mate") ;
+    //alert("Button is clicked mate") ;
+
+    switch(this.innerHTML) {
+      case 'w':
+        var audio = new Audio('sounds/crash.mp3') ;
+        audio.play() ;
+        break ;
+      case 'a':
+        audio = new Audio('sounds/kick-bass.mp3') ;
+        audio.play() ;
+        break ;
+      case 's':
+        audio = new Audio('sounds/snare.mp3') ;
+        audio.play() ;
+        break ;
+      case 'd':
+        audio = new Audio('sounds/tom-1.mp3') ;
+        audio.play() ;
+        break ;
+      case 'j':
+        audio = new Audio('sounds/tom-2.mp3') ;
+        audio.play() ;
+        break ;
+      case 'k':
+          audio = new Audio('sounds/tom-3.mp3') ;
+          audio.play() ;
+          break ;
+      case 'l':
+          audio = new Audio('sounds/tom-4.mp3') ;
+          audio.play() ;
+          break ;
+      default:
+        break ;
+    }
+
+
     })
 }
+
+document.addEventListener("keydown" , function(e) {
+    switch(e.key) {
+      case 'w':
+        var audio = new Audio('sounds/crash.mp3') ;
+        audio.play() ;
+        break ;
+        case 'a':
+          audio = new Audio('sounds/kick-bass.mp3') ;
+          audio.play() ;
+          break ;
+        case 's':
+          audio = new Audio('sounds/snare.mp3') ;
+          audio.play() ;
+          break ;
+        case 'd':
+          audio = new Audio('sounds/tom-1.mp3') ;
+          audio.play() ;
+          break ;
+        case 'j':
+          audio = new Audio('sounds/tom-2.mp3') ;
+          audio.play() ;
+          break ;
+        case 'k':
+            audio = new Audio('sounds/tom-3.mp3') ;
+            audio.play() ;
+            break ;
+        case 'l':
+            audio = new Audio('sounds/tom-4.mp3') ;
+            audio.play() ;
+            break ;
+        default:
+          break ;
+    }
+
+}) ;
